fbb6843a2b9b33f1b02cad92ff0cd2154b324f82
Zach Gregory <zachgregorync@gmail.com>
2021-11-09 14:38:03 -0500

    create memory game

---
+++ app.js
@@ -0,0 +1,9 @@
+//Grab a couple of things we need
+const section = document.querySelector('section');
+const playerLivesCount = document.querySelector("span");
+const playerLives =  6;
+
+//Link text
+playerLivesCount.textContent = playerLives;
+
+//Generate the data
